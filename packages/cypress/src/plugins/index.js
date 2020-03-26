608013b6f170f041319a1a2067a33c923a807df2
Sean Smith <sean.smith.2009@gmail.com>
2020-03-26 13:46:44 +0800

    Fix cypress setup

--- packages/cypress/src/plugins/index.js
+++ packages/cypress/src/plugins/index.js
@@ -11,7 +11,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-module.exports = (on, config) => {
+const wp = require("@cypress/webpack-preprocessor");
+
+module.exports = on => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
@@ -25,4 +27,10 @@ module.exports = (on, config) => {
     }
     return args;
   });
+
+  // Webpack setup
+  const options = {
+    webpackOptions: require("../../webpack.config"),
+  };
+  on("file:preprocessor", wp(options));
 };
