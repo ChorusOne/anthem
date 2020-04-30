53b744bb614dc00825bb3a042c8af1cb67b4b846
Sean Smith <sean.smith.2009@gmail.com>
2020-04-30 17:53:32 +0800

    Update celo-ledger-utils

--- packages/client/src/tools/celo-ledger-utils.ts
+++ packages/client/src/tools/celo-ledger-utils.ts
@@ -2,8 +2,16 @@ import Eth from "@ledgerhq/hw-app-eth";
 import TransportU2F from "@ledgerhq/hw-transport-u2f";
 import TransportUSB from "@ledgerhq/hw-transport-webusb";
 
+const Kit = require("@celo/contractkit");
+
 /** ===========================================================================
  * Celo Ledger Utils
+ * ----------------------------------------------------------------------------
+ * References:
+ * - Celo Ledger App: https://docs.celo.org/celo-gold-holder-guide/ledger
+ * - Contract Kit: https://www.npmjs.com/package/@celo/contractkit
+ * - Contract Kit Guide: https://docs.celo.org/developer-guide/start/hellocelo
+ * - Ledger ETH App: https://www.npmjs.com/package/@ledgerhq/hw-app-eth
  * ============================================================================
  */
 
@@ -17,12 +25,24 @@ const getCeloLedgerTransport = () => {
   throw new Error("Browser not supported!");
 };
 
+const checkAddressWithContractKit = async (address: string) => {
+  try {
+    console.log("Checking contract kit for balance!");
+    const kit = Kit.newKit("https://alfajores-forno.celo-testnet.org");
+    const balance = await kit.web3.eth.getBalance(address);
+    console.log(balance);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const connectCeloAddress = async () => {
   try {
-    const transport = getCeloLedgerTransport();
+    const transport = await getCeloLedgerTransport();
     const eth = new Eth(transport);
     const { address } = await eth.getAddress("44'/52752'/0'/0/1", true);
     console.log(`Got Celo Address! ${address}`);
+    checkAddressWithContractKit(address);
   } catch (error) {
     console.error(error);
   }
@@ -30,7 +50,7 @@ export const connectCeloAddress = async () => {
 
 export const signCeloTransaction = async (transactionData: any) => {
   try {
-    const transport = getCeloLedgerTransport();
+    const transport = await getCeloLedgerTransport();
     const eth = new Eth(transport);
     const { address } = await eth.signTransaction(
       "44'/52752'/0'/0/0",
