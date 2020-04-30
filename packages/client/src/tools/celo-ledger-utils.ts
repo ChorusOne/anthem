f53947f13c84d46253b65638cc9798f8571f7c31
Sean Smith <sean.smith.2009@gmail.com>
2020-04-30 18:08:30 +0800

    Uninstall contractkit

--- packages/client/src/tools/celo-ledger-utils.ts
+++ packages/client/src/tools/celo-ledger-utils.ts
@@ -2,13 +2,12 @@ import Eth from "@ledgerhq/hw-app-eth";
 import TransportU2F from "@ledgerhq/hw-transport-u2f";
 import TransportUSB from "@ledgerhq/hw-transport-webusb";
 
-const Kit = require("@celo/contractkit");
-
 /** ===========================================================================
  * Celo Ledger Utils
  * ----------------------------------------------------------------------------
  * References:
  * - Celo Ledger App: https://docs.celo.org/celo-gold-holder-guide/ledger
+ * - Celo Example Web App: https://github.com/celo-org/celo-ledger-web-app/blob/master/index.js
  * - Contract Kit: https://www.npmjs.com/package/@celo/contractkit
  * - Contract Kit Guide: https://docs.celo.org/developer-guide/start/hellocelo
  * - Ledger ETH App: https://www.npmjs.com/package/@ledgerhq/hw-app-eth
@@ -25,24 +24,12 @@ const getCeloLedgerTransport = () => {
   throw new Error("Browser not supported!");
 };
 
-const checkAddressWithContractKit = async (address: string) => {
-  try {
-    console.log("Checking contract kit for balance!");
-    const kit = Kit.newKit("https://alfajores-forno.celo-testnet.org");
-    const balance = await kit.web3.eth.getBalance(address);
-    console.log(balance);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 export const connectCeloAddress = async () => {
   try {
     const transport = await getCeloLedgerTransport();
     const eth = new Eth(transport);
     const { address } = await eth.getAddress("44'/52752'/0'/0/1", true);
     console.log(`Got Celo Address! ${address}`);
-    checkAddressWithContractKit(address);
   } catch (error) {
     console.error(error);
   }
