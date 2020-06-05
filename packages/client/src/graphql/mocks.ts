1e5167116f78c0e065bf9267bb743045284074ad
Sean Smith <sean.smith.2009@gmail.com>
2020-06-05 15:35:24 +0800

    Rebuild resolver keys

--- packages/client/src/graphql/mocks.ts
+++ packages/client/src/graphql/mocks.ts
@@ -20,6 +20,7 @@ import { loader } from "graphql.macro";
 const schemaString = loader("../../../server/src/schema/schema.graphql");
 
 const keys = RESOLVER_QUERY_KEYS as ReadonlyArray<QueryKeyUnion>;
+console.log(keys);
 
 /** ===========================================================================
  * Types & Config
@@ -97,6 +98,7 @@ const getQueryResolverFromKey = (key: QueryKeyUnion) => async (
 
     // Get saved response data from utils/ package client/data directory
     const json = require(`../../../utils/src/client/data/${key}.json`);
+    console.log(json);
 
     // Return response JSON
     return json[key];
