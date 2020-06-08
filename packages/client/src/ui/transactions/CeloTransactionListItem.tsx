0d356ce08b759804ca45578a9cbf154d836d7df5
Sean Smith <sean.smith.2009@gmail.com>
2020-06-08 16:57:43 +0800

    WIP - rendering Celo transactions

--- packages/client/src/ui/transactions/CeloTransactionListItem.tsx
+++ packages/client/src/ui/transactions/CeloTransactionListItem.tsx
@@ -1,11 +1,22 @@
 import { ICeloTransaction, NetworkDefinition } from "@anthem/utils";
 import { Card, Elevation } from "@blueprintjs/core";
+import { OasisGenericEvent } from "assets/images";
 import { FiatCurrency } from "constants/fiat";
 import { ILocale } from "i18n/catalog";
 import Modules from "modules/root";
 import React from "react";
+import { formatAddressString } from "tools/client-utils";
 import { TranslateMethodProps } from "tools/i18n-utils";
-import { TransactionCardStyles } from "./TransactionComponents";
+import AddressIconComponent from "ui/AddressIconComponent";
+import {
+  ClickableEventRow,
+  EventContextBox,
+  EventIconBox,
+  EventRow,
+  EventRowItem,
+  EventText,
+  TransactionCardStyles,
+} from "./TransactionComponents";
 
 /** ===========================================================================
  * Types & Config
@@ -32,9 +43,69 @@ interface IProps extends TranslateMethodProps {
 class CeloTransactionListItem extends React.PureComponent<IProps, {}> {
   render(): Nullable<JSX.Element> {
     return (
-      <Card style={TransactionCardStyles} elevation={Elevation.TWO}></Card>
+      <Card style={TransactionCardStyles} elevation={Elevation.TWO}>
+        <EventRow data-cy="transaction-list-item">
+          {this.renderBlockNumber()}
+          {this.renderAddressBlocks()}
+        </EventRow>
+      </Card>
     );
   }
+
+  renderBlockNumber = () => {
+    const { blockNumber } = this.props.transaction;
+    return (
+      <EventRowItem style={{ minWidth: 215 }}>
+        <EventIconBox>
+          <OasisGenericEvent />
+        </EventIconBox>
+        <EventContextBox>
+          <EventText style={{ fontWeight: "bold" }}>Block Number</EventText>
+          <EventText data-cy="transaction-block-number">
+            {blockNumber}
+          </EventText>
+        </EventContextBox>
+      </EventRowItem>
+    );
+  };
+
+  renderAddressBlocks = () => {
+    const { from, to } = this.props.transaction;
+    if (from && to) {
+      return (
+        <>
+          {this.renderAddressBox(from, "From")}
+          {this.renderAddressBox(to, "To")}
+        </>
+      );
+    }
+
+    return null;
+  };
+
+  renderAddressBox = (address: string, titleText: string) => {
+    return (
+      <ClickableEventRow onClick={this.handleLinkToAddress(address)}>
+        <EventIconBox>
+          <AddressIconComponent
+            address={address}
+            networkName={this.props.network.name}
+            validatorOperatorAddressMap={{}}
+          />
+        </EventIconBox>
+        <EventContextBox>
+          <EventText style={{ fontWeight: "bold" }}>{titleText}</EventText>
+          <EventText style={{ fontWeight: 100, fontSize: 12 }}>
+            {formatAddressString(address, true)}
+          </EventText>
+        </EventContextBox>
+      </ClickableEventRow>
+    );
+  };
+
+  handleLinkToAddress = (address: string) => () => {
+    this.props.setAddress(address, { showToastForError: true });
+  };
 }
 
 /** ===========================================================================
