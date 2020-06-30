3f3b093fad6bacac1980b5d43441046ae6c94b9f
Sean Smith <sean.smith.2009@gmail.com>
2020-06-30 16:12:51 +0800

    Add staking test for Celo network

--- packages/cypress/src/integration/staking.spec.ts
+++ packages/cypress/src/integration/staking.spec.ts
@@ -1,7 +1,7 @@
 import { NETWORK_ADDRESS_DEFAULTS } from "@anthem/utils";
 import { SCREEN_SIZES, UTILS, APP_URL } from "../support/cypress-utils";
 
-const { COSMOS } = NETWORK_ADDRESS_DEFAULTS;
+const { COSMOS, CELO } = NETWORK_ADDRESS_DEFAULTS;
 
 /** ===========================================================================
  * Test the staking/ page
@@ -27,6 +27,29 @@ SCREEN_SIZES.forEach(({ size, type }) => {
 
       if (type.isDesktop()) {
         UTILS.checkForNetwork("cosmos");
+      }
+    });
+  });
+
+  describe("Anthem supports staking for Celo networks", () => {
+    beforeEach(() => {
+      UTILS.setViewportSize(size);
+      UTILS.loginWithAddress(type, "celo");
+    });
+
+    afterEach(() => {
+      UTILS.logout(type);
+    });
+
+    it("After logging in with an address the staking page is accessible", () => {
+      cy.visit(`${APP_URL}/celo/delegate/${CELO.account}`);
+      cy.contains("Staking");
+      cy.contains("Validator Group");
+      cy.contains("Voting Power");
+      cy.contains("Capacity");
+
+      if (type.isDesktop()) {
+        UTILS.checkForNetwork("celo");
       }
     });
   });
