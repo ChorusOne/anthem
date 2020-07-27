e420735026c80d302b6e06b8bd2ef1b77ad56cb9
Sean Smith <sean.smith.2009@gmail.com>
2020-07-27 14:55:29 +0800

    Render only group address in transaction setup

--- packages/client/src/ui/workflows/CeloTransactionWorkflows.tsx
+++ packages/client/src/ui/workflows/CeloTransactionWorkflows.tsx
@@ -1074,7 +1074,7 @@ class CreateTransactionForm extends React.Component<IProps, IState> {
     const group = selectedValidatorForDelegation as ICeloValidatorGroup;
 
     const data = {
-      group,
+      group: group.group,
       from: address,
       amount: unitToDenom(amount, denominationSize),
     };
