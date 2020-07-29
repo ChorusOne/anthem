1736b76801a14b3185f93d543313d449b9e8d5fa
Sean Smith <sean.smith.2009@gmail.com>
2020-07-29 15:28:38 +0800

    Add disclaimer text

--- packages/client/src/ui/pages/PolkadotPage.tsx
+++ packages/client/src/ui/pages/PolkadotPage.tsx
@@ -10,7 +10,7 @@ import { connect } from "react-redux";
 import styled from "styled-components";
 import { composeWithProps } from "tools/context-utils";
 import PageAddressBar from "ui/PageAddressBar";
-import { Button, PageContainer } from "ui/SharedComponents";
+import { Button, Link, PageContainer } from "ui/SharedComponents";
 
 /** ===========================================================================
  * Types & Config
@@ -43,19 +43,33 @@ class PolkadotPage extends React.Component<IProps, IState> {
     return (
       <PageContainer>
         <PageAddressBar pageTitle="Polkadot Staking Agent" />
-        <ContentArea>
-          <Title>
-            Start earning 8.99% APR on Polkadot with our Staking Agent.
-          </Title>
-          <Text>
-            The Chorus One Staking Agent optimizes your DOT returns on Polkadot
-            by automatically diversifying across validators and periodically
-            withdrawing staking rewards for you.*
-          </Text>
-          <Button style={{ marginTop: 24 }} onClick={this.handleActivateAgent}>
-            <b>ACTIVATE AGENT</b>
-          </Button>
-        </ContentArea>
+        <Row>
+          <ContentArea>
+            <Title>
+              Start earning 8.99% APR on Polkadot with our Staking Agent.
+            </Title>
+            <Text>
+              The Chorus One Staking Agent optimizes your DOT returns on
+              Polkadot by automatically diversifying across validators and
+              periodically withdrawing staking rewards for you.<b>*</b>
+            </Text>
+            <Button
+              style={{ marginTop: 24 }}
+              onClick={this.handleActivateAgent}
+            >
+              <b>ACTIVATE AGENT</b>
+            </Button>
+          </ContentArea>
+          <ContentArea>OK</ContentArea>
+        </Row>
+        <Disclaimer>
+          <b>*</b> Activating the Staking Agent will enable Chorus One to carry
+          out staking-related transactions on your behalf saving you time and
+          gas fees in the process. In the MVP version, the Staking Agent will
+          ensure you are optimally staking by maintaining an updated list of
+          Chorus One validators. To learn more, check out the introduction
+          article <Link href="https://chorus.one">here</Link>.
+        </Disclaimer>
       </PageContainer>
     );
   }
@@ -81,6 +95,18 @@ const Text = styled.p`
   font-size: 15px;
   font-weight: 200;
   margin-top: 18px;
+`;
+
+const Disclaimer = styled.p`
+  position: absolute;
+  bottom: 75px;
+  padding-right: 75px;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 
 /** ===========================================================================
